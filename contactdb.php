21168be310023a10e73890a01c9e02f364dc4add
YousifAltaher <240133485@aston.ac.uk>
2025-11-25 11:30:51 +0000

    contact us page and php for that code

--- contactdb.php
+++ contactdb.php
@@ -5,13 +5,11 @@ $db_name = 'team40db';
 $username = 'root';
 $password = '';
 
-try{
-
-    $db = new PDO("mysql:dbname=$db_name;host=$db_host", $username, $password);    
-    echo("Successfully connected to the database.<br>");
-}catch(PDOexception $ex) {
+try {
+    $db = new PDO("mysql:dbname=$db_name;host=$db_host", $username, $password);
+} catch (PDOException $ex) {
     echo("Failed to connect to the database.<br>");
-    echo($ex->getmessage());
+    echo($ex->getMessage());
     exit;
 }
 
