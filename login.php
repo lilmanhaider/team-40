2ce252a1a67581229c6959f879d53310c8405ab0
lilmanhaider <mhhaiderh@gmail.com>
2025-11-25 11:27:08 +0000

    .

--- login.php
+++ login.php
@@ -40,7 +40,7 @@ if (isset($_POST['submitted'])) {
             background-color: lightblue;
             }
 
-            h2 {
+            h2{
             color: black;
             text-align: center;
             }
