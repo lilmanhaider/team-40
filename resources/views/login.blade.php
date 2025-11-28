b0b5f2c67b5304a782a32669383946600c764d15
Mohammed Haider Hussain <240136660@aston.ac.ukssh-keygen -t rsa -C 240136660@aston.ac.uk>
2025-11-28 18:13:52 +0000

    Create login.blade.php

---
+++ resources/views/login.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<body>
+    <br>
+    <h2>Log In</h2>
+    <br>
+
+    @if($errors->any())
+        <div class="error">{{ $errors->first() }}</div>
+    @endif
+
+    <form method="POST" action="{{ route('login.post') }}">
+        @csrf
+        <p>Username: <input type="text" name="email" required /></p>
+        <p>Password: <input type="password" name="password" required /></p>
+        <br>
+        <input type="submit" value="Log In" />
+    </form>
+</body>
+</html>
