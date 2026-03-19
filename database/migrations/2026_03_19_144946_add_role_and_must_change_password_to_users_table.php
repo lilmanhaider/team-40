99f5ac76b996f29af15d9b0f1b99395cfb890053
Mohammed Haider Hussain <240136660@aston.ac.ukssh-keygen -t rsa -C 240136660@aston.ac.uk>
2026-03-19 14:55:00 +0000

    start of admin

---
+++ database/migrations/2026_03_19_144946_add_role_and_must_change_password_to_users_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+{
+    Schema::table('users', function (Blueprint $table) {
+        $table->string('role')->default('customer');
+        $table->boolean('must_change_password')->default(false);
+        //
+    });
+}
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down()
+    {
+    Schema::table('users', function (Blueprint $table) {
+        $table->dropColumn(['role', 'must_change_password']);
+    });
+
+    }
+};
